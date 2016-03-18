18e3a6db76d1fee3c65053920359bb8fb9d23b38
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-18 08:26:23 -0500

    Updated test for showAllResults

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* global createResultShopItem */
+/* global createResultShopItem showAllResults */
 
 QUnit.module(`Etsy Shop`, () => {
   const itemOne = {
@@ -29,91 +29,84 @@ QUnit.module(`Etsy Shop`, () => {
     },
   };
 
-  test(`it can create a shopItem element`, (assert) => {
-    const info = itemOne;
-
-    // Check the shop item element
-    const shopItem = createResultShopItem(info);
-    assert.ok(shopItem instanceof Element,
-      `The createResultShopItem function should return an Element object
+  function testUiForItem(el, item, assert, msgPrefix) {
+    assert.ok(el instanceof Element,
+      `${msgPrefix}: The shop item should be an Element object
         (see document.createElement)`);
-    assert.ok(shopItem.classList.contains(`shop-item`),
-      `The returned element should have a class 'shop-item'`);
+    assert.ok(el.classList.contains(`shop-item`),
+      `${msgPrefix}: The shop item element element should have a class 'shop-item'`);
 
     // Check the title
-    const title = shopItem.querySelector(`h3.shop-item__title`);
+    const title = el.querySelector(`h3.shop-item__title`);
     assert.ok(title,
-      `The returned element contains an element with the class 'shop-item__title'`);
-    assert.equal(title.innerText.trim(), info.title,
-      `The shop item title contains the shop item's title from the data`);
+      `${msgPrefix}: The shop item contains an element with the class 'shop-item__title'`);
+    assert.equal(title.innerText.trim(), item.title,
+      `${msgPrefix}: The shop item title contains the shop item's title from the data`);
 
     // Check the shop name
-    const shopName = shopItem.querySelector(`h4.shop-item__shop-name`);
+    const shopName = el.querySelector(`h4.shop-item__shop-name`);
     assert.ok(shopName,
-      `The returned element contains an element with the class 'shop-item__shop-name'`);
-    assert.equal(shopName.innerText.trim(), info.Shop.shop_name,
-      `The shop item shopName contains the shop item's shop name from the data`);
+      `${msgPrefix}: The shop item contains an element with the class 'shop-item__shop-name'`);
+    assert.equal(shopName.innerText.trim(), item.Shop.shop_name,
+      `${msgPrefix}: The shop item shop-name contains the shop item's shop name from the data`);
 
     // Check shop item picture
-    const pic = shopItem.querySelector(`img.shop-item__pic`);
+    const pic = el.querySelector(`img.shop-item__pic`);
     assert.ok(pic,
-      `The returned element contains an 'img' element with the class 'shop-item__pic'`);
-    assert.equal(pic.getAttribute(`src`), info.Images[0].url_fullxfull,
-      `The shop item pic has an src from the first Image url`);
-    assert.equal(pic.getAttribute(`alt`), info.title,
-      `The shop item pic has an alt from the shop item's name`);
+      `${msgPrefix}: The shop item contains an 'img' element with the class 'shop-item__pic'`);
+    assert.equal(pic.getAttribute(`src`), item.Images[0].url_fullxfull,
+      `${msgPrefix}: The shop item pic has an src from the first Image url`);
+    assert.equal(pic.getAttribute(`alt`), item.title,
+      `${msgPrefix}: The shop item pic has an alt from the shop item's name`);
 
     // Check shop item price
-    const price = shopItem.querySelector(`.shop-item__price`);
+    const price = el.querySelector(`.shop-item__price`);
     assert.ok(price,
-      `The returned element contains an 'p' element with the class 'shop-item__price'`);
-    assert.equal(price.innerText.trim(), `$1.01`,
-      `The shop item price contains the shop item's price from the data`);
+      `${msgPrefix}: The shop item contains an 'p' element with the class 'shop-item__price'`);
+    assert.equal(price.innerText.trim(), `$${item.price}`,
+      `${msgPrefix}: The shop item price contains the shop item's price from the data`);
+  }
+
+  test(`free passing test`, (assert) => {
+    assert.ok(true);
   });
 
-  test(`it can create a result shopItem element from the data`, (assert) => {
-    const info = itemTwo;
-
+  test(`it can create a shopItem element`, (assert) => {
     // Check the shop item element
-    const shopItem = createResultShopItem(info);
-    assert.ok(shopItem instanceof Element,
-      `The createResultShopItem function should return an Element object
-        (see document.createElement)`);
-    assert.ok(shopItem.classList.contains(`shop-item`),
-      `The returned element should have a class 'shop-item'`);
+    const shopItem = createResultShopItem(itemOne);
 
-    // Check the title
-    const title = shopItem.querySelector(`h3.shop-item__title`);
-    assert.ok(title,
-      `The returned element contains an element with the class 'shop-item__title'`);
-    assert.equal(title.innerText.trim(), info.title,
-      `The shop item title contains the shop item's title from the data`);
+    testUiForItem(shopItem, itemOne, assert,
+      `Result of createResultShopItem with itemOne`);
 
-    // Check the shop name
-    const shopName = shopItem.querySelector(`h4.shop-item__shop-name`);
-    assert.ok(shopName,
-      `The returned element contains an element with the class 'shop-item__shop-name'`);
-    assert.equal(shopName.innerText.trim(), info.Shop.shop_name,
-      `The shop item shopName contains the shop item's shop name from the data`);
+    const shopItemTwo = createResultShopItem(itemTwo);
 
-    // Check shop item picture
-    const pic = shopItem.querySelector(`img.shop-item__pic`);
-    assert.ok(pic,
-      `The returned element contains an 'img' element with the class 'shop-item__pic'`);
-    assert.equal(pic.getAttribute(`src`), info.Images[0].url_fullxfull,
-      `The shop item pic has an src from the first Image url`);
-    assert.equal(pic.getAttribute(`alt`), info.title,
-      `The shop item pic has an alt from the shop item's name`);
-
-    // Check shop item price
-    const price = shopItem.querySelector(`.shop-item__price`);
-    assert.ok(price,
-      `The returned element contains an 'p' element with the class 'shop-item__price'`);
-    assert.equal(price.innerText.trim(), `$50.19`,
-      `The shop item price contains the shop item's price from the data`);
+    testUiForItem(shopItemTwo, itemTwo, assert,
+      `Result of createResultShopItem with itemTwo`);
   });
 
   test(`it can update the UI with results from a set of data`, (assert) => {
+    const products = document.createElement(`div`);
+    document.querySelector(`body`).appendChild(products);
+    products.id = `products`;
 
+    showAllResults({ results: [itemOne, itemTwo] });
+
+    const shopItemOne = products.querySelector(`.shop-item`);
+    const shopItemTwo = products.querySelector(`.shop-item:last-of-type`);
+
+    testUiForItem(shopItemOne, itemOne, assert,
+      `Result of showAllResults for itemOne`);
+
+    testUiForItem(shopItemTwo, itemTwo, assert,
+      `Result of showAllResults for itemOne`);
+
+    showAllResults({ results: [itemTwo] });
+
+    const shopItemReset = products.querySelector(`.shop-item`);
+
+    testUiForItem(shopItemReset, itemTwo, assert,
+      `showAllResults should reset products children`);
+
+    products.remove();
   });
 });
