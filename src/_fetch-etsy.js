b26821f02b07bd819e275aab0ac101a53fe51bc2
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-18 12:03:22 -0500

    Update to fix CORS issue

--- src/_fetch-etsy.js
+++ src/_fetch-etsy.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function jsonp(url) {
+  return new Promise((resolve) => {
+    const callbackName = `jsonp_callback_${Math.round(100000 * Math.random())}`;
+    const script = document.createElement(`script`);
+    window[callbackName] = function (data) {
+      delete window[callbackName];
+      document.body.removeChild(script);
+      resolve(data);
+    };
+
+    const urlBreaker = url.indexOf(`?`) >= 0 ? `&` : `?`;
+    const src = `${url}${urlBreaker}callback=${callbackName}`;
+
+    script.src = src;
+    document.body.appendChild(script);
+  });
+}
+
+
 /**
  * Makes a request to Etsy's API and returns a promise with JSON Data
  * @param  String searchTerm
@@ -7,12 +26,8 @@
  */
 function fetchEtsy(searchTerm) {
   const apiKey = `ki0carhy6hn0ldkouxbju1wq`;
-  const url = `https://api.etsy.com/v2/listings/active.js?` +
+  const url = `https://openapi.etsy.com/v2/listings/active.js?` +
     `api_key=${apiKey}&keywords=${searchTerm}&includes=Images,Shop&sort_on=score`;
 
-  return fetch(url, {
-    mode: `no-cors`,
-  }).then((res) => {
-    return res.json;
-  });
+  return jsonp(url);
 }
